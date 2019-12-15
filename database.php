b48a5d783da02a12b4bf6710f2b7633c99f5ac31
knp56 <51336521+knp56@users.noreply.github.com>
2019-12-15 00:52:30 -0500

    Create database.php

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+
+$username='bcn3_proj';
+$password='w9hLvRFP';
+$dsn="mysql:host=sql1.njit.edu;dbname=$username";
+try
+{
+$db= new PDO($dsn,$username,$password);
+}
+catch (PDOException $e)
+{
+    $error_message = $e->getMessage();
+    include('../errors/database_error.php');
+    exit();
+}
+?>
