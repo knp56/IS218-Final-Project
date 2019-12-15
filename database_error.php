4f107c5ecb117614b9b5b69e85d40dd545201579
knp56 <51336521+knp56@users.noreply.github.com>
2019-12-15 00:53:01 -0500

    Create database_error.php

---
+++ database_error.php
@@ -0,0 +1,11 @@
+<?php include '../view/header.php'; ?>
+
+<div id="main">
+    <h1> Database Errors </h1>
+    <p class="first_paragraph"> Error regarding the database.  </p>
+    < class="last_paragraph"> Error message: <?php echo $error_message; ?></p>
+
+</div>
+
+<?php include '../view/footer.php'; ?>
+
