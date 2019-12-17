933b14d31d6972bbf91c607c6ddbeacda4f5c4e9
knp56 <51336521+knp56@users.noreply.github.com>
2019-12-16 22:34:39 -0500

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,99 +1,64 @@
-<?php
-
-require('pdo.php');
-
-$fname= filter_input(INPUT_POST, 'fname');
-$lname= filter_input(INPUT_POST, 'lname');
-$birthday=filter_input(INPUT_POST, 'birthday');
-$email= filter_input(INPUT_POST, 'email');
-$password= filter_input(INPUT_POST, 'password');
-$doubleCheck=strpos($email,'a');
-
-if ($_SERVER["REQUEST_METHOD"]=="POST")
-{
-    $Validate=true;
-    if (empty($fname)) {
-        $firstNameError = "Must provide your first name.";
-        $Validate=false;
-    }
-    if (empty($lname)) {
-        $lastNameError = "Must provide your last name.";
-        $Validate=false;
-    }
-    if (empty($birthday)) {
-        $birthdayError = "Must provide your birthday.";
-        $Validate=false;
-    }
-    if (empty($email)) {
-        $emailError = "Must type in a valid email.";
-        $Validate=false;
-    } elseif ($doubleCheck == false) {
-        $emailError = "Email is not valid.";
-        $Validate=false;
-    }
-    if (empty($password)) {
-        $passwordError = "Must type in a valid password.";
-        $Validate=false;
-    } elseif (strlen($password) <= 8) {
-        $passwordError = "Password must be at least 8 characters long.";
-        $Validate=false;
-    }
-
-
-
-
-    if($Validate==true)
-    {
-        $query = 'INSERT INTO accounts
-                     (email, fname, lname, birthday, password)
-                VALUES 
-                    (:email, :fname, :lname, :birthday, :password)';
-
-
-        $statement = $db->prepare($query);
-
-        $statement->bindValue(':email',$email);
-        $statement->bindValue(':fName',$fname);
-        $statement->bindValue(':lName',$lname);
-        $statement->bindValue(':birthday',$birthday);
-        $statement->bindValue(':password',$password);
-
-        $statement->execute();
-        $statement->closeCursor();
-
-        header('Location: ../login.html');
-    }
-
-}
-?>
-
+<!DOCTYPE html>
 <html lang="en">
-
-<head><title> Registration Form </title></head>
+<head>
+    <meta charset="UTF-8">
+    <title>Registration</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 <body>
 
-<h2> Registration Information </h2>
+<main>
 
-<div>
-    First Name: <?php echo $fname; ?>
-    <span <span class="error"> <?php echo $firstNameError; ?> </span>
-</div>
-<div>
-    Last Name: <?php echo $lname; ?>
-    <span <span class="error"> <?php echo $lastNameError; ?> </span>
-</div>
-<div>
-    Birthday: <?php echo $birthday; ?>
-    <span <span class="error"> <?php echo $birthdayError; ?> </span>
-</div>
-<div>
-    Email: <?php echo $email; ?>
-    <span <span class="error"> <?php echo $emailError;?></span>
-</div>
-<div>
-    Password: <?php if (!$password) echo $password; ?>
-    <span <span class="error"> <?php echo $passwordError;?></span>
-</div>
+    <div class="registrationform">
+
+    <h1> Registration Form </h1>
+
+
+    <form action="index.php" method="post">
+        <input type="hidden" name="action" value="create_user">
+
+    <div class="form-group">
+        <label for="fname">First Name</label>
+        <input type="text" class="form-control" name="fname" id="fname">
+
+        <br><br>
+
+         <class="form-group">
+            <label for="lname"> Last Name</label>
+            <input type="text" class="form-control" name="lname" id="lname">
+
+<br><br>
+
+         <class="form-group">
+            <label for="birthday">Birthday </label>
+            <input type="date" class="form-control" name="birthday" id="birthday">
+
+        <br><br>
+
+         <class="form-group">
+            <label for="email"> Email </label>
+            <input type="email" class="form-control" name="email" id="email">
+
+        <br><br>
+
+         <class="form-group">
+            <label for="password"> Password </label>
+            <input type="password" class="form-control" name="password" id="password">
+    </div>
+
+        <button type="submit" value="register">Register</button>
+
+        <br><br><br><br>
+
+        <label> &nbsp;</label>
+        <button type="button"><a href="login.html">Login Page</a></button>
+        <br>
+        <button type="button"><a href="questions.html">Questions</a></button>
+        <br>
+        <button type="button"><a href="registration.php">Registration</a></button>
+    </form>
+    </div>
+</main>
 </body>
 </html>
 
