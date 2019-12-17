1bdaabb038a0dac7db0f753f804cc00113b27d03
knp56 <51336521+knp56@users.noreply.github.com>
2019-12-16 22:21:50 -0500

    Update database_error.php

--- database_error.php
+++ database_error.php
@@ -1,11 +1,7 @@
-<?php include '../view/header.php'; ?>
+<?php ?>
 
 <div id="main">
     <h1> Database Errors </h1>
-    <p class="first_paragraph"> Error regarding the database.  </p>
-    < class="last_paragraph"> Error message: <?php echo $error_message; ?></p>
+    <p class="last_paragraph"> Error: <?php echo $error_message; ?></p>
 
 </div>
-
-<?php include '../view/footer.php'; ?>
-
