b0b3d91559fa69cdf5495c1ff75638c6d451997e
knp56 <51336521+knp56@users.noreply.github.com>
2019-12-16 22:30:16 -0500

    Update and rename login.html to index.php

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <style></style>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<?php
+require('database.php');
+require('accounts_db.php');
+require('questions_db.php');
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL)
+    {
+        $action = 'show_display';
+    }
+}
+switch ($action) {
+    case 'show_display': {
+        include('display.php');
+        break;
+    }
+    case 'validate_display': {
+        $email = filter_input(INPUT_POST, 'email');
+        $password = filter_input(INPUT_POST, 'password');
+        if ($email == NULL || $password == NULL) {
+            $error = "Email/Password not included";
+            include('error.php');
+        } else {
+            $userid = validate_display($email, $password);
+            echo "USER ID IS: $userid";
+            if ($userid == false) {
+                echo "Go to Registration";
+                header("Location: .?action=display_registration.php");
+            } else {
+                header("Location: .?action=display_question&userid=$userid");
+            }
+        }
+        break;
+    }
+    case 'display_registration': {
+        include('registration.php');
+        break;
+    }
+    case 'create_user':{
+        $email = filter_input(INPUT_POST, 'email');
+        $fname = filter_input(INPUT_POST, 'fname');
+        $lname = filter_input(INPUT_POST, 'lname');
+        $birthday = filter_input(INPUT_POST, 'birthday');
+        $password = filter_input(INPUT_POST, 'password');
+        if($email == NULL|| $fname == NULL || $lname == NULL || $birthday == NULL|| ($password == NULL || strlen($password) <= 8)){
+            $error = "Information is incorrect.";
+            echo $error;
+        }
+        else{
+            new_user($email, $fname, $lname, $birthday, $password);
+            header("Location: .?action=show_display");
+        }
+        break;
+    }
+    case 'display_question': {
+        $userid = filter_input(INPUT_GET, 'userid');
+        if ($userid == NULL || $userid < 0) {
+            header('Location: .?action=display_display');
+        } else {
+            $question = get_users_question($userid);
+            include('questList.php');
+        }
+        break;
+    }
+    case 'display_questForm': {
+        $userid = filter_input(INPUT_GET, 'userid');
+        session_start();
+        $_SESSION['userid'] = $userid;
+        include('question.php');
+        break;
+    }
+    case 'create_new_question':{
+        session_start();
+        $userid = $_SESSION['userid'];
+
+        $questionName = filter_input(INPUT_POST, 'questionName');
+        $questionBody = filter_input(INPUT_POST, 'questionBody');
+        $questionSkills = filter_input(INPUT_POST, 'questionSkills');
+        $CheckSkills = explode(',', $questionSkills);
+
+        if ($questionName == NULL || ($questionBody == NULL || strlen($questionBody) >= 500) || ($questionSkills == NULL || count($CheckSkills) < 2)){
+            $error = "Information is incorrect!";
+            echo $error;
+        }
+        else{
+            new_question($userid, $questionName, $questionBody, $questionSkills);
+            header("Location: .?action=display_question&userid=$userid");
+        }
+        break;
+    }
+    case 'delete_question':{
+        $userid = $_SESSION['userid'];
+        $questionId = filter_input(INPUT_POST, 'questionId');
+        $userid = filter_input(INPUT_POST, 'userid');
+        delete_question($questionId);
+        header("Location: .?action=display_question&userid=$userid");
+        break;
+    }
+    default: {
+        $error = 'Unknown Action';
+        include('error.php');
+    }
+}?>
+</body>
+</html>
