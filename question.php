88141d612ff0a44f1b8e09d68908a9360558fa92
knp56 <51336521+knp56@users.noreply.github.com>
2019-12-20 21:39:37 -0500

    Update question.php

--- question.php
+++ question.php
@@ -7,43 +7,29 @@
 </head>
 <body>
 <div class="questionForm">
-
-
-
-<h1> Question Form </h1>
+    <h1> Question Form </h1>
     <form action="index.php" method="post">
         <input type="hidden" name="action" value="create_new_question">
-
         <div>
-
-        <class="form-group">
+            <class="form-group">
             <label for="questionName"> Question Name </label>
             <input type="text" class="form-control" name="questionName" id="questionName">
-
-<br><br>
-
-
-        <class="form-group">
+            <br><br>
+            <class="form-group">
             <label for="questionBody"> Question Body </label>
             <input type="text" class="form-control" name="questionBody" id="questionBody">
-<br><br>
-
-        <class="form-group">
+            <br><br>
+            <class="form-group">
             <label for="questionSkills"> Question Skills </label>
             <input type="text" class="form-control" name="questionSkills" id="questionSkills">
-
         </div>
-
     </form>
-
-
     <label> &nbsp;</label>
     <button type="button"><a href="display.php">Login Page</a></button>
     <br>
     <button type="button"><a href="question.php">Questions</a></button>
     <br>
     <button type="button"><a href="registration.php">Registration</a></button>
-
 </div>
 </body>
 </html>
