ed361eaa33d049f5594686b3788bdf76b80d2a36
knp56 <51336521+knp56@users.noreply.github.com>
2019-12-20 21:35:12 -0500

    Update accounts_db.php

--- accounts_db.php
+++ accounts_db.php
@@ -1,18 +1,14 @@
 <?php
-
 function validate_display($email,$password)
 {
     global $db;
     $query='SELECT * FROM accounts WHERE email=:email AND password=:password';
-
     $statement=$db->prepare($query);
     $statement->bindValue(':email',$email);
     $statement->bindValue(':password',$password);
     $statement->execute();
-
     $user=$statement->fetch();
     $isValiddisplay=count($user)>0;
-
     if(!$isValiddisplay)
     {
         $statement->closeCursor();
@@ -25,25 +21,19 @@ function validate_display($email,$password)
         return $userid;
     }
 }
-
 function get_username($userid)
 {
-global $db;
-$query='SELECT fname, lname FROM accounts WHERE id=:userid';
-$statement=$db->prepare($query);
-$statement->bindValue(':userid',$userid);
-
-$statement->execute();
-$names=$statement->fetch();
-
-$statement->closeCursor();
-$firstname=$names['fname'];
-$secondname=$names['lname'];
-
-return $firstname . '' .$secondname;
+    global $db;
+    $query='SELECT fname, lname FROM accounts WHERE id=:userid';
+    $statement=$db->prepare($query);
+    $statement->bindValue(':userid',$userid);
+    $statement->execute();
+    $names=$statement->fetch();
+    $statement->closeCursor();
+    $firstname=$names['fname'];
+    $secondname=$names['lname'];
+    return $firstname . '' .$secondname;
 }
-
-
 function new_user($email,$fname,$lname,$birthday, $password)
 {
     global $db;
@@ -51,15 +41,12 @@ function new_user($email,$fname,$lname,$birthday, $password)
 (email, fname, lname, birthday, password)
 VALUES
 (:email, :fname, :lname, :birthday, :password)';
-
     $statement=$db->prepare($query);
-
     $statement->bindValue(':email',$email);
     $statement->bindValue(':fname',$fname);
     $statement->bindValue(':lname',$lname);
-    $statement->bindValue('birthday', $birthday);
+    $statement->bindValue(':birthday',$birthday);
     $statement->bindValue(':password',$password);
-
     $statement->execute();
     $statement->closeCursor();
 }
