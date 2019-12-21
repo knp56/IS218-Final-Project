16d06f958803b4ad19f4bcc026788d26ea099bf7
knp56 <51336521+knp56@users.noreply.github.com>
2019-12-20 21:40:04 -0500

    Update questions_db.php

--- questions_db.php
+++ questions_db.php
@@ -1,21 +1,50 @@
 <?php
-
-function get_users_question ($ownerid)
+function get_user_question ($ownerid)
 {
-global $db;
-
+    global $db;
     $query = ' SELECT * FROM question WHERE ownerid = :ownerid';
-
     $statement = $db->prepare($query);
-
     $statement->bindValue(':ownerid', $ownerid);
     $statement->execute();
-
-    $questions=$statement->fetchAll();
+    $question=$statement->fetchAll();
     $statement->closeCursor();
-
-    return $questions;
+    return $question;
 }
+
+
+
+function singleQuest($id)
+{
+    global $db;
+    $query="SELECT * FROM question WHERE id=:id";
+    $statement= $db->prepare($query);
+    $statement->bindValue(':id',$id);
+    $statement->execute();
+    $question=$statement->fetchAll();
+    $statement->closeCursor();
+    return $question;
+}
+
+
+
+function all_user_question()
+{
+    global $db;
+    $query = "SELECT * FROM question";
+
+    $statement=$db->prepare($query);
+    $statement->execute();
+    $question=$statement->fetchAll();
+    $statement->closeCursor();
+    return $question;
+}
+
+
+
+
+
+
+
 
 function new_question($userid,$questionName,$questionBody,$questionSkills)
 {
@@ -24,22 +53,14 @@ function new_question($userid,$questionName,$questionBody,$questionSkills)
 (ownerid,title,body,skills)
 VALUES
 (:ownerid,:title,:body,:skills)';
-
     $statement=$db->prepare($query);
-
-    $statement->bindValue('ownerid',$userid);
+    $statement->bindValue(':ownerid',$userid);
     $statement->bindValue(':title',$questionName);
     $statement->bindValue(':body',$questionBody);
     $statement->bindValue(':skills',$questionSkills);
-
     $statement->execute();
     $statement->closeCursor();
-
 }
-
-
-
-
 function delete_question($id)
 {
     global $db;
