2ff946b33dd9d3b25df22b8f277a1afd0fa17918
knp56 <51336521+knp56@users.noreply.github.com>
2019-12-20 21:37:45 -0500

    Update display.php

--- display.php
+++ display.php
@@ -6,53 +6,36 @@
     <meta charset="UTF-8">
     <title>Login Form</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-
 </head>
 <body>
-
-
 <main>
-<div class="loginForm">
-    <form action="index.php" method="post">
+    <div class="loginForm">
+        <form action="index.php" method="post">
 
-      
             <h1>Login Form</h1>
+            <div>
+                <input type="hidden" name="action" value="validate_display">
+                <class="form-group">
+                <label for="email">Email </label>
+                <input type="email" class="form-control" name="email" id="email">
+                <br><br>
+                <class="form-group">
+                <label for="password"> Password </label>
+                <input type="password" class="form-control" name="password" id="password">
+                <button type="submit" value="login"> Submit </button>
+                <p> Click below to Register </p>
+                <button type="button"><a href="registration.php">Registration</a></button>
+                <br><br><br><br>
+                <label> &nbsp;</label>
+                <button type="button"><a href="display.php">Login Page</a></button>
+                <br>
+                <button type="button"><a href="question.php">Questions</a></button>
+                <br>
+            </div>
+        </form>
 
 
-        <div>
-
-        <input type="hidden" name="action" value="validate_display">
-
-         <class="form-group">
-            <label for="email">Email </label>
-            <input type="email" class="form-control" name="email" id="email">
-
-        <br><br>
-
-        <class="form-group">
-            <label for="password"> Password </label>
-            <input type="password" class="form-control" name="password" id="password">
-
-
-
-        <button type="submit" value="login"> Submit </button>
-
-
-
-
-        <p> Click below to Register </p>
-        <button type="button"><a href="registration.php">Registration</a></button>
-<br><br><br><br>
-        <label> &nbsp;</label>
-        <button type="button"><a href="display.php">Login Page</a></button>
-        <br>
-        <button type="button"><a href="question.php">Questions</a></button>
-        <br>
-
-</div>
-
-    </form>
-</div>
+    </div>
 </main>
 </body>
 </html>
