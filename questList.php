584bd3942b06c53179495173761da2f99c608de5
knp56 <51336521+knp56@users.noreply.github.com>
2019-12-20 21:40:58 -0500

    Update questList.php

--- questList.php
+++ questList.php
@@ -8,48 +8,64 @@
 <body>
 <main>
 
-
-
-
-<?php ?>
-
-
     <h1> Question <?php echo get_username($userid); ?>
-        $question = get_users_question($userid);
+        $question = get_user_question($userid);
     </h1>
 
 
-<a href=".?action=questList&userid=<?php echo $userid ?>"><input type="submit" value="Add a new question"></a>
-
+    <a href=".?action=questList&userid=<?php echo $userid ?>"><input type="submit" value="Add a new question"></a>
+<a href=".?action=otherQuestions&userid=<?php echo $userid; ?>"> Other Questions </a>
     <table align="center">
-    <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Body</th>
-        <th>Skills</th>
-        <th>Delete</th>
-    </tr>
-    <?php foreach($questionId as $question) : ?>
-    <tr>
-        <td><?php echo $question['id'];?></td>
-        <td><?php echo $question['title'];?></td>
-        <td><?php echo $question['body'];?></td>
-        <td><?php echo $question['skills'];?></td>
-        <td>
-            <form action="index.php" method="post">
-                <input type="hidden" name="action" value="deleteQuestion">
-                <input type="hidden" name="questionId" value="<?php echo $question['id'];?>">
-                <input type="hidden" name="userid" value="<?php echo $userid ?>">
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Body</th>
+            <th>Skills</th>
+            <th>View</th>
+            <th>Delete</th>
+        </tr>
+        <?php foreach($question as $question) : ?>
+            <tr>
+                <td><?php echo $question['id'];?></td>
+                <td><?php echo $question['title'];?></td>
+                <td><?php echo $question['body'];?></td>
+                <td><?php echo $question['skills'];?></td>
+                <td>
+                    <form>
+                        <input type="hidden" name="action" value="singleQuest">
+                        <input type="hidden" name="questionId" value="<?php echo $question['id'];?>">
+                        <input type="hidden" name="userid" value="<?php echo $userid ?>">
 
-                <button><input class="btn" type="submit" value="Delete" ></button>
+                        <button><input type="submit" class="btn edit" value="View" ></button>
 
-             </form>
-        </td>
-    </tr>
-<?php endforeach; ?>
-        </table>
-<a href=".?action=show_login"><input type="button" value="LogOut"></a>
+                    </form>
+                </td>
+                <td>
+                    <form action="index.php" method="post">
+                        <input type="hidden" name="action" value="delete_question">
+                        <input type="hidden" name="questionId" value="<?php echo $question['id'];?>">
+                        <input type="hidden" name="userid" value="<?php echo $userid?>">
 
+                        <button><input class="btn" type="submit" value="Delete"> </button>
+                    </form>
+
+                </td>
+
+
+
+
+
+            </tr>
+        <?php endforeach; ?>
+    </table>
+    <a href=".?action=display.php"><input type="button" value="LogOut"></a>
+
+    <label> &nbsp;</label>
+    <button type="button"><a href="display.php">Login Page</a></button>
+    <br>
+    <button type="button"><a href="question.php">Questions</a></button>
+    <br>
+    <button type="button"><a href="registration.php">Registration</a></button>
 
 </main>
 </body>
